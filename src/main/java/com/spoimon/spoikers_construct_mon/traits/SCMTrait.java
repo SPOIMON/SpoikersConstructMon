f0ec7d82109457d1734a71c4cd19447d8c9950fb
riku1227 <17927874+riku1227@users.noreply.github.com>
2021-08-25 19:35:58 +0900

    特性にコメントを追加

--- src/main/java/com/spoimon/spoikers_construct_mon/traits/SCMTrait.java
+++ src/main/java/com/spoimon/spoikers_construct_mon/traits/SCMTrait.java
@@ -3,6 +3,10 @@ package com.spoimon.spoikers_construct_mon.traits;
 import net.minecraft.util.text.TextFormatting;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 
+/**
+ * SCMで追加される特性のベースクラス
+ * IDに"scm_"が自動的に追加される
+ */
 public class SCMTrait extends AbstractTrait {
     public SCMTrait(String identifier, TextFormatting color) {
         super("scm_" + identifier, color);
--- src/main/java/com/spoimon/spoikers_construct_mon/traits/TraitSoft.java
+++ src/main/java/com/spoimon/spoikers_construct_mon/traits/TraitSoft.java
@@ -3,6 +3,14 @@ package com.spoimon.spoikers_construct_mon.traits;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 
+/**
+ * ・柔らか
+ *     - ふにゃふにゃ！
+ *     - 耐久度を2倍消費する
+ *
+ * デメリット特性
+ * 耐久度を2倍消費する
+ */
 public class TraitSoft extends SCMTrait {
     public TraitSoft() {
         super("soft", 0xddebed);
